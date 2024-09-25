0fccfae2d7a06b92219760b6f7157004b63e3956
Jiawei9527 <200607810@student.georgianc.on.ca>
2024-09-25 15:52:08 -0400

    comment 1 test update

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -7,7 +7,8 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("this is a test for github push update.");
+
     }
 
     // Update is called once per frame
